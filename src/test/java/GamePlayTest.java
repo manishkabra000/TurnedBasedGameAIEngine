61579452c2fa5cb762c4db9a869fb49f0b914290
Manish Kabra <manishkabra000@gmail.com>
2025-05-13 22:50:03 +0530

    unit test update

--- src/test/java/GamePlayTest.java
+++ src/test/java/GamePlayTest.java
@@ -4,7 +4,6 @@ import game.*;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import java.util.Scanner;
 
 public class GamePlayTest {
     GameEngine gameEngine;
@@ -19,37 +18,21 @@ public class GamePlayTest {
     }
     @Test
     public void checkForRowWin() {
-        Board board = gameEngine.start("TikTakToe");
-
-        // Human vs AI
-        Player human = new Player("X");
-        Player ai = new Player("0");
-
-        int next = 0;
-        int[][] moves = new int[][] {{1,0},{1,1},{1,2}};
-
-        while (!ruleEngine.getState(board).isOver()) {
-            System.out.println("Make you move!");
-            // For human -> get input from the user i.e. my human
-            int row = moves[next][0];
-            int col = moves[next][1];
-            next++;
-            gameEngine.move(board, new Move(new Cell(row, col), human));
-            System.out.println(board.toString());
-
-            // For AI -> suggest input
-            if(!ruleEngine.getState(board).isOver()) {
-                Move aiMove = aiPlayer.suggestMove(ai, board);
-                gameEngine.move(board, aiMove);
-                System.out.println(board);
-            }
-        }
+        Board board;
+        board = gamePlay(new int[][] {{1,0},{1,1},{1,2}});
         Assert.assertTrue(ruleEngine.getState(board).isOver());
         Assert.assertEquals("X", ruleEngine.getState(board).getWinner());
     }
 
     @Test
     public void checkForColWin() {
+        Board board;
+        board = gamePlay(new int[][] {{0,0},{1,0},{2,0}});
+        Assert.assertTrue(ruleEngine.getState(board).isOver());
+        Assert.assertEquals("X", ruleEngine.getState(board).getWinner());
+    }
+
+    private Board gamePlay(int[][] moves) {
         Board board = gameEngine.start("TikTakToe");
 
         // Human vs AI
@@ -57,7 +40,6 @@ public class GamePlayTest {
         Player ai = new Player("0");
 
         int next = 0;
-        int[][] moves = new int[][] {{0,0},{1,0},{2,0}};
 
         while (!ruleEngine.getState(board).isOver()) {
             System.out.println("Make you move!");
@@ -65,7 +47,6 @@ public class GamePlayTest {
             int row = moves[next][0];
             int col = moves[next][1];
             next++;
-
             gameEngine.move(board, new Move(new Cell(row, col), human));
             System.out.println(board.toString());
 
@@ -76,7 +57,7 @@ public class GamePlayTest {
                 System.out.println(board);
             }
         }
-        Assert.assertTrue(ruleEngine.getState(board).isOver());
-        Assert.assertEquals("X", ruleEngine.getState(board).getWinner());
+
+        return board;
     }
 }
