b981198233050ce3bcbffb0fccaf6a7ca87fc2b6
Manish Kabra <manishkabra000@gmail.com>
2025-05-18 19:19:30 +0530

    isDiagVictory DRY

--- src/main/java/api/RuleEngine.java
+++ src/main/java/api/RuleEngine.java
@@ -5,12 +5,17 @@ import game.Board;
 import game.GameResult;
 
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class RuleEngine {
     public GameResult getState(Board board) {
         if(board instanceof TikTakToeBoard board1) {
             BiFunction<Integer,Integer, String> getNextRow = board1::getCell;
             BiFunction<Integer,Integer, String> getNextCol = (i, j) -> board1.getCell(j, i);
+
+            Function<Integer, String> getNextDiag = (i) -> board1.getCell(i, i);
+            Function<Integer, String> getNextRevDiag = (i) -> board1.getCell(2 - i, i);
+
             // 1. Row Wise
             GameResult rowResult = isVictory(getNextRow);
             if (rowResult.isOver()) return rowResult;
@@ -20,34 +25,12 @@ public class RuleEngine {
             if (colResult.isOver()) return colResult;
 
             // 3. Diagonal
-            String firstChar;
-            firstChar = board1.getCell(0,0);
-            boolean diagComplete = firstChar != null;
-            if(firstChar != null) {
-                for (int i = 0; i < 3; i++) {
-                    if (!firstChar.equals(board1.getCell(i, i))) {
-                        diagComplete = false;
-                        break;
-                    }
-                }
-            }
-            if(diagComplete) {
-                return new GameResult(true, firstChar);
-            }
+            GameResult diagResult = isDiagStreak(getNextDiag);
+            if(diagResult.isOver()) return diagResult;
+
             // 4. Reverse Diagonal
-            firstChar = board1.getCell(2,0);
-            boolean revDiagComplete = firstChar != null;
-            if(firstChar != null) {
-                for(int i = 0; i < 3; i++) {
-                    if (!firstChar.equals(board1.getCell(2 - i,i))) {
-                        revDiagComplete = false;
-                        break;
-                    }
-                }
-            }
-            if(revDiagComplete) {
-                return new GameResult(true, firstChar);
-            }
+            GameResult revDiagResult = isDiagStreak(getNextRevDiag);
+            if(revDiagResult.isOver()) return revDiagResult;
 
             int countOfFilledCells = 0;
             for (int i = 0; i < 3; i++) {
@@ -66,6 +49,20 @@ public class RuleEngine {
         } else {
             return new GameResult(false, "-");
         }
+    }
+
+    private static GameResult isDiagStreak(Function<Integer, String> next) {
+        boolean isStreak = true;
+        for (int i = 0; i < 3; i++) {
+            if (next.apply(i) == null || !next.apply(0).equals(next.apply(i))) {
+                isStreak = false;
+                break;
+            }
+            if(isStreak) {
+                return new GameResult(true, next.apply(0));
+            }
+        }
+        return new GameResult(false, "-");
     }
 
     private static GameResult isVictory(BiFunction<Integer,Integer, String> next) {
