3f28be1ae328de73f611105cd4254d12620a7a1d
Manish Kabra <manishkabra000@gmail.com>
2025-06-02 13:08:40 +0530

    GameInfo class created

--- src/main/java/game/GameInfo.java
+++ src/main/java/game/GameInfo.java
@@ -1,10 +1,7 @@
 package game;
 
 public class GameInfo {
-    private boolean isOver;
-    private String winner;
 
     public GameInfo(GameState gameState, boolean hasFork, Player player) {
-
     }
 }
